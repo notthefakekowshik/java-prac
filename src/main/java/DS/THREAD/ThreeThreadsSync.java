47793eca4c595636eb108f13b8ed381544cd6c8b
knarayanam <knarayanam@workfusion.com>
2023-11-11 17:21:38 +0530

    Sync with 3 threas, NICE EXAMPLE

---
+++ src/main/java/DS/THREAD/ThreeThreadsSync.java
@@ -0,0 +1,102 @@
+//package DS.THREAD;
+//
+//class ThreeThreadsImpl implements Runnable{
+//
+//    int counter = 1;
+//    private static final Object lock = new Object();
+//
+//    @Override
+//    public void run() {
+//        synchronized (lock) {
+//            while(counter < 20) {
+//                if(Thread.currentThread().getName().equals("two") && counter % 2==0) {
+//                    System.out.println(Thread.currentThread().getName() + "-->" + counter);
+//                    counter +=1;
+//                }
+//                else if(Thread.currentThread().getName().equals("three") && counter % 3==0) {
+//                    System.out.println(Thread.currentThread().getName() + "-->" + counter);
+//                    counter +=1;
+//                }
+//                else if(Thread.currentThread().getName().equals("five") && counter % 5==0) {
+//                    System.out.println(Thread.currentThread().getName() + "-->" + counter);
+//                    counter +=1;
+//                }
+//                else {
+//                    try {
+//                        lock.wait();
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//                lock.notifyAll();
+//            }
+//        }
+//
+//    }
+//}
+//public class ThreeThreadsSync {
+//    public static void main(String[] args) {
+//        ThreeThreadsImpl threeThreads = new ThreeThreadsImpl();
+//
+//        Thread two = new Thread(threeThreads, "two");
+//        Thread three = new Thread(threeThreads, "three");
+//        Thread five = new Thread(threeThreads, "five");
+//
+//        two.start();
+//        three.start();
+//        five.start();
+//
+//
+//    }
+//}
+package DS.THREAD;
+
+class ThreeThreadsImpl implements Runnable {
+
+    private int counter = 2;
+    private static final Object lock = new Object();
+
+    @Override
+    public void run() {
+        synchronized (lock) {
+            while (counter <= 20) {
+                try {
+                    if (Thread.currentThread().getName().equals("two") && counter % 2 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "-->" + counter);
+                        counter += 1;
+                        lock.notifyAll();
+                    } else if (Thread.currentThread().getName().equals("three") && counter % 3 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "-->" + counter);
+                        counter += 1;
+                        lock.notifyAll();
+                    } else if (Thread.currentThread().getName().equals("five") && counter % 5 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "-->" + counter);
+                        counter += 1;
+                        lock.notifyAll();
+                        // what if I use lock.notify() in all the cases.
+                    } else if (counter%2!=0 && counter %3 !=0 && counter%5!=0) {
+                        counter+=1;
+                    } else {
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
+
+public class ThreeThreadsSync {
+    public static void main(String[] args) {
+        ThreeThreadsImpl threeThreads = new ThreeThreadsImpl();
+
+        Thread two = new Thread(threeThreads, "two");
+        Thread three = new Thread(threeThreads, "three");
+        Thread five = new Thread(threeThreads, "five");
+
+        two.start();
+        three.start();
+        five.start();
+    }
+}
